7a6199cb32a296db7868d53f75898a2e9f90c2a7
David Holdeman <dave@holdemanenterprises.com>
2024-02-13 07:42:52 -0600

    test makeover-bundle with compile_firmware

---
+++ compile_firmware.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export BUNDLE_NAME="paralela"
+export PROJECT_CPU="ARCH_STM32F4"
--- compile_paralela.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PROJECT_BOARD="paralela"
-export PROJECT_CPU="ARCH_STM32F4"
-export SHORT_BOARD_NAME="paralela"
