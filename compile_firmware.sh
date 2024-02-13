9cfcd1e831d383ca97d05520548361761cab70f6
David Holdeman <dave@holdemanenterprises.com>
2024-02-12 20:53:09 -0600

    test makeover-bundle

--- compile_firmware.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ext/rusefi/firmware/
-
-bash config/boards/common_script.sh ../../../../.. ARCH_STM32F4
---
+++ compile_paralela.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PROJECT_BOARD="paralela"
+export PROJECT_CPU="ARCH_STM32F4"
+export SHORT_BOARD_NAME="paralela"
