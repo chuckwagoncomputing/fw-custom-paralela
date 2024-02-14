1e2377372cdbac4e6673fdfac5dffb9c1fd2da71
David Holdeman <dave@holdemanenterprises.com>
2024-02-13 21:32:51 -0600

    don't include meta-info

--- board.mk
+++ board.mk
@@ -2,9 +2,6 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 
 BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
-# defines SHORT_BOARD_NAME
-include $(BOARD_DIR)/meta-info.env
-
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
