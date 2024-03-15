50883c6fdab5ec208d022f66f5cfef8eb3ef02fd
David Holdeman <dave@holdemanenterprises.com>
2024-03-15 17:32:48 -0500

    remove for reference files

--- generated/signature_paralela.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2192944932
-#define TS_SIGNATURE "rusEFI default.2023.12.10.paralela.2192944932"
