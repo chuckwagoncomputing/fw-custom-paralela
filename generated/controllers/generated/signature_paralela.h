da493391bb2a2d25f7a61b29d27c903148b4aec5
GitHub git update Action <action@github.com>
2024-04-25 17:30:28 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_paralela.h
+++ generated/controllers/generated/signature_paralela.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2451538613
-#define TS_SIGNATURE "rusEFI default.2024.04.24.paralela.2451538613"
+#define SIGNATURE_HASH 1468624155
+#define TS_SIGNATURE "rusEFI default.2024.04.25.paralela.1468624155"
