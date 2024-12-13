10c11b891cef071067165196b5a5011d110b483a
GitHub git update Action <action@github.com>
2024-12-13 17:34:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/engine_configuration_generated_structures_paralela.h
+++ generated/controllers/generated/engine_configuration_generated_structures_paralela.h
@@ -6085,8 +6085,12 @@ struct persistent_config_s {
 	 * offset 25056
 	 */
 	scaled_channel<uint8_t, 1, 10> minimumOilPressureValues[8];
+	/**
+	 * offset 25064
+	 */
+	blend_table_s targetAfrBlends[TARGET_AFR_BLEND_COUNT];
 };
-static_assert(sizeof(persistent_config_s) == 25064);
+static_assert(sizeof(persistent_config_s) == 25440);
 
 // end
 // this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt
--- generated/controllers/generated/signature_paralela.h
+++ generated/controllers/generated/signature_paralela.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2035792079
-#define TS_SIGNATURE "rusEFI master.2024.12.12.paralela.2035792079"
+#define SIGNATURE_HASH 1216999816
+#define TS_SIGNATURE "rusEFI master.2024.12.13.paralela.1216999816"
